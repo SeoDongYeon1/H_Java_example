c5ecc619a8245dff2f8d847ae46d14d18743bf6c
SeoDongYeon1 <ehddus2655@gmail.com>
2023-03-16 21:50:07 +0900

    게시판 만들기 베이스

---
+++ src/com/KoreaIT/java/ex/Util.java
@@ -0,0 +1,17 @@
+package com.KoreaIT.java.ex;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class Util {
+	/** 포맷팅 현재 날짜/시간 반환 Str*/
+	public static String getNowDate() {
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+ 
+        Date now = new Date();
+ 
+        String nowDate = sdf.format(now);
+        
+		return nowDate;
+	}
+}
